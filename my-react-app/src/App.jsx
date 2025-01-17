fde80856f159be216e4320b233382239809012c4
Dhvanit <dhvanit03@gmail.com>
2025-01-17 18:13:00 +0530

    Initial commit while working on frontend using react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './App.css'
+import Square from './Square/Square'
+
+const renderFrom=[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+const App=()=>{
+  return (
+    <div className='main-div'>
+      <div>
+        <div className='move-detection'>
+          <div className='left'>YourSelf</div>
+          <div className='right'>Opponent</div>
+        </div>
+        <h1 className='game-heading water-background'>Tic Tac Toe</h1>
+        <div className='square-wrapper'>
+          {
+            renderFrom.map((arr)=>
+            arr.map((e)=>{
+              return <Square/>
+            }))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ my-react-app/src/Square/Square.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Square.css'
+const Square=()=>{
+    return (
+        <div className='square'>Square</div>
+    )
+}
+
+export default Square
